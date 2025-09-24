18fc679f31bb679ed6a938830ae0f068d4e08d73
WojciechMitera <miterawojciech@gmail.com>
2025-09-24 10:52:08 +0200

    commit1

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "figma")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "figma.Pages")]
